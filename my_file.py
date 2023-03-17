2530cc9a3db1c5151f65a17d63fc308618d40374
Jordan Rainone <47578985+rainonej@users.noreply.github.com>
2023-03-17 13:44:26 -0700

    moved stuff

---
+++ my_file.py
@@ -0,0 +1,20 @@
+import argparse
+
+# Initialize argument parser
+parser = argparse.ArgumentParser(description='Process training data.')
+
+# Add arguments
+parser.add_argument('--training_images', type=str, required=True, help='Path to training images YAML file.')
+parser.add_argument('--training_params', type=str, required=True, help='Path to training parameters YAML file.')
+parser.add_argument('--output_dictionary_path', type=str, required=True, help='Path to output dictionary pickle file.')
+
+# Parse arguments
+args = parser.parse_args()
+import yaml
+with open(args.training_params, 'r') as file:
+    my_data = yaml.load(file, Loader=yaml.FullLoader)
+
+# Access arguments
+print(args.training_images)
+print(my_data)
+print(args.output_dictionary_path)
